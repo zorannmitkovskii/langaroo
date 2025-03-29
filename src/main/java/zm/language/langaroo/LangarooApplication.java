7ed976bb494595fc4a797aea35f1e661ffc2ecf6
zoranmitkovski <z.mitkovski@heartforhealth.com>
2025-03-29 16:40:33 +0100

    Initial commit

---
+++ src/main/java/zm/language/langaroo/LangarooApplication.java
@@ -0,0 +1,13 @@
+package zm.language.langaroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangarooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangarooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zm/language/langaroo/LangarooApplicationTests.java
@@ -0,0 +1,13 @@
+package zm.language.langaroo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangarooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
